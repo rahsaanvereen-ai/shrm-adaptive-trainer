2abc1565075e350ffde519a9cd440d498c657715
rahsaanvereen-ai <rahsaanvereen@gmail.com>
2026-08-26 17:13:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: "supabaseUrl: "http://usecbdumlqopfvfbdkqn.supabase.co"
+  supabaseUrl: "http://usecbdumlqopfvfbdkqn.supabase.co"
   supabasePublishableKey: "sb_publishable_wTRrKY1Z6SdNvEuTk8dBcg_FotRBuyM",
   appName: "SHRM Adaptive Trainer"
 };
